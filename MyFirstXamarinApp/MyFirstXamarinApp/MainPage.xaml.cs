f86490138199700b318aef6c24417be0124605dd
brody-rebne <bsrebne@gmail.com>
2020-05-11 14:30:24 -0700

    Revert "hello world"
    This reverts commit 6cfa1534ca00bcab78682accea3b29ccd7f530d8.

--- MyFirstXamarinApp/MyFirstXamarinApp/MainPage.xaml.cs
+++ MyFirstXamarinApp/MyFirstXamarinApp/MainPage.xaml.cs
@@ -17,12 +17,5 @@ namespace MyFirstXamarinApp
         {
             InitializeComponent();
         }
-
-        int count = 0;
-        void Button_Clicked(object sender, System.EventArgs e)
-        {
-            count++;
-            ((Button)sender).Text = $"You clicked {count} times.";
-        }
     }
 }
